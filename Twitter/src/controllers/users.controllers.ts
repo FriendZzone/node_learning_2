ebf4a72340eaf6c45671a3de93aaeff032e334bb
Dư Thanh Được <duthanhduoc@gmail.com>
2023-06-07 13:55:50 +0700

    Tạo wrapRequestHandler để xử lý lỗi

--- Twitter/src/controllers/users.controllers.ts
+++ Twitter/src/controllers/users.controllers.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { RegisterReqBody } from '~/models/requests/User.requests'
 import User from '~/models/schemas/User.schema'
@@ -17,17 +17,14 @@ export const loginController = (req: Request, res: Response) => {
   })
 }
 
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
-  try {
-    const result = await usersService.register(req.body)
-    return res.json({
-      message: 'Register success',
-      result
-    })
-  } catch (error) {
-    return res.status(400).json({
-      message: 'Register failed',
-      error
-    })
-  }
+export const registerController = async (
+  req: Request<ParamsDictionary, any, RegisterReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await usersService.register(req.body)
+  return res.json({
+    message: 'Register success',
+    result
+  })
 }
