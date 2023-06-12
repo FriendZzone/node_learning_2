f706bbb966e3440675df657177d1c077d1e70315
Dư Thanh Được <duthanhduoc@gmail.com>
2023-06-12 15:03:23 +0700

    Verify forgot password

--- Twitter/src/models/requests/User.requests.ts
+++ Twitter/src/models/requests/User.requests.ts
@@ -26,6 +26,9 @@ export interface ForgotPasswordReqBody {
   email: string
 }
 
+export interface VerifyForgotPasswordReqBody {
+  forgot_password_token: string
+}
 export interface TokenPayload extends JwtPayload {
   user_id: string
   token_type: TokenType
