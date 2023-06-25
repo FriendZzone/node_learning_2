f463ec0a9ee401a6be390bb41fcf94d5e2d97c73
Dư Thanh Được <duthanhduoc@gmail.com>
2023-06-25 19:42:40 +0700

    Upload file bằng formidable

--- Twitter/src/index.ts
+++ Twitter/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { pick } from 'lodash'
 import { defaultErrorHandler } from '~/middlewares/error.middlewares'
+import mediasRouter from '~/routes/medias.routes'
 import usersRouter from '~/routes/users.routes'
 import databaseService from '~/services/database.services'
 databaseService.connect()
@@ -8,6 +9,7 @@ const app = express()
 const port = 4000
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 app.use(defaultErrorHandler)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
