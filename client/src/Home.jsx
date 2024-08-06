3b76f692db7d4c6181418a25e2c38565bde0c7f8
datdh <datdh@omegatheme.com>
2024-08-06 15:37:07 +0700

    update client

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -11,7 +11,6 @@ import {
   MediaPoster
 } from '@vidstack/react'
 
-console.log(import.meta.env)
 const getGoogleAuthUrl = () => {
   const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env
   const url = `https://accounts.google.com/o/oauth2/v2/auth`
